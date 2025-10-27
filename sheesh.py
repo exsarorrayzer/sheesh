30eb5dc78a716eb044b61e20437965d3b30afbce
exsarorrayzer <theleapy@gmail.com>
2025-10-28 01:16:44 +0400

    Create sheesh.py

---
+++ sheesh.py
@@ -0,0 +1,11 @@
+from utils.banner import show_banner
+from utils.creds import show_creds
+from utils.obs_main import start_menu
+
+def main():
+    show_banner()
+    show_creds()
+    start_menu()
+
+if __name__ == "__main__":
+    main()
