103983cbc6c48de69c25a1f7a79d2e5ae72c6c8e
exsarorrayzer <theleapy@gmail.com>
2025-10-28 09:28:14 +0400

    Update comment_adder.py

--- obsufucators/comment_adder.py
+++ obsufucators/comment_adder.py
@@ -2,20 +2,21 @@
 import hashlib
 import random
 import string
+import time
+
+random.seed(time.time_ns())
 
 def _random_str(min_len=16, max_len=32):
     length = random.randint(min_len, max_len)
     return ''.join(random.choices(string.ascii_letters, k=length))
 
 def obfuscate(source: str) -> str:
-    lines = source.splitlines()
     new_lines = []
-    for line in lines:
-        stripped = line.rstrip()
-        if stripped == "":
+    for line in source.splitlines():
+        if not line.strip():
             new_lines.append(line)
             continue
         rand_text = _random_str()
-        md5_hash = hashlib.md5(rand_text.encode("utf-8")).hexdigest()
-        new_lines.append(f"{stripped}  # {md5_hash}")
+        md5_hash = hashlib.md5(rand_text.encode()).hexdigest()
+        new_lines.append(f"{line}  # {md5_hash}")
     return "\n".join(new_lines)
